8847677a00d5eced93b3b62260f70e424a22de53
our ame <you@example.com>
2026-02-18 02:09:41 +0300

    basa

---
+++ users/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'phone', 'city', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name', 'phone')
+    ordering = ('email',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Персональная информация', {'fields': ('first_name', 'last_name', 'phone', 'city', 'avatar')}),
+        ('Права доступа', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Важные даты', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+            'email', 'password1', 'password2', 'first_name', 'last_name', 'phone', 'city', 'avatar', 'is_staff',
+            'is_active')}
+         ),
+    )
---
+++ users/models.py
@@ -0,0 +1,72 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db import models
+
+
+class UserManager(BaseUserManager):
+    """Менеджер для модели User без username"""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Создает и сохраняет обычного пользователя
+        """
+        if not email:
+            raise ValueError('Email обязателен')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """
+        Создает и сохраняет суперпользователя
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Суперпользователь должен иметь is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Суперпользователь должен иметь is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    username = None  # Убираем поле username
+    email = models.EmailField(unique=True, verbose_name='Email')
+    phone = models.CharField(max_length=35, verbose_name='Телефон', blank=True, null=True)
+    city = models.CharField(max_length=100, verbose_name='Город', blank=True, null=True)
+    avatar = models.ImageField(upload_to='users/avatars/', verbose_name='Аватар', blank=True, null=True)
+
+    # Явно указываем related_name для групп и разрешений
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name='groups',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        related_name='custom_user_set',
+        related_query_name='custom_user',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name='user permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_name='custom_user_set',
+        related_query_name='custom_user',
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []  # Убираем username из обязательных полей
+
+    objects = UserManager()  # Добавляем наш менеджер
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.email
